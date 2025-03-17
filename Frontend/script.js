9d585478f7ad964aacf15a1a8ea6fdb380ee9600
Officialdevpro <officialdevpro@gmail.com>
2025-03-17 12:10:05 +0530

    first commit

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,15 +1,3 @@
-const nav = document.querySelector(".nav");
-window.addEventListener("scroll", fixNav);
-
-function fixNav() {
-  if (window.scrollY > nav.offsetHeight + 150) {
-    nav.classList.add("active");
-  } else {
-    nav.classList.remove("active");
-  }
-}
-
-// Set event start date (YYYY, MM-1, DD, HH, MM, SS)
 const eventDate = new Date("2025-04-10T00:00:00").getTime();
 
 function updateCountdown() {
@@ -45,49 +33,22 @@ function updateCountdown() {
 setInterval(updateCountdown, 1000);
 updateCountdown();
 
+let slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
 
-
-
-
-const hero = document.querySelector(".hero");
-
-// Array of images
-const images = [
-"../Frontend/assets/images/2T0A4377-min.jpg",
-"../Frontend/assets/images/8L7A4741-min.jpg",
-"../Frontend/assets/images/8L7A4780-min.jpg",
-"../Frontend/assets/images/bg2-min.jpg",
-
-];
-
-let index = 0;
-
-function changeBackground() {
-index = (index + 1) % images.length; // Loop through images
-hero.style.backgroundImage = `url(${images[index]})`;
+function showNextSlide() {
+  slides[currentSlide].classList.remove("active");
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].classList.add("active");
 }
 
-// Change image every 5 seconds
-setInterval(changeBackground, 3000);
+setInterval(showNextSlide, 5000);
 
-
-
-let slides = document.querySelectorAll('.slide');
-        let currentSlide = 0;
-
-        function showNextSlide() {
-            let previousSlide = currentSlide;
-            currentSlide = (currentSlide + 1) % slides.length;
-
-            gsap.timeline()
-                .to(slides[previousSlide], { opacity: 0, y: currentSlide === 1 ? -100 : 100, duration: 1.2, ease: 'power2.inOut' })
-                .set(slides[previousSlide], { opacity: 0, x: 100 })
-                .fromTo(
-                    slides[currentSlide],
-                    { opacity: 0, x: currentSlide === 0 ? -100 : currentSlide === 1 ? 0 : 100, y: currentSlide === 1 ? -100 : 100 },
-                    { opacity: 1, x: 0, y: 0, duration: 1.5, ease: 'expo.out' }
-                );
-        }
-
-        gsap.set(slides[0], { opacity: 1, x: 0 });
-        setInterval(showNextSlide, 5000);
+let hamMenus = document.querySelectorAll(".ham-menu");
+hamMenus.forEach((menu) => {
+  menu.addEventListener("click", () => {
+    document.querySelectorAll(".nav-ham").forEach((nav) => {
+      nav.classList.remove("visible");
+    });
+  });
+});
